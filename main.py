9edf95b9c9c6aa7b1bb86338d9bf0bddf1ec7363
dallas <deast@ithaca.edu>
2026-09-15 13:53:02 -0400

    Added Display menu to main

---
+++ main.py
@@ -0,0 +1,7 @@
+def display_menu():
+    print("\n === Personal Library Manager===")
+    print("1. Add a book title")
+    print("2. Remove a book title")
+    print("3. List all book titles")
+    print("4. Search for a book title")
+    print("5. Exit")
