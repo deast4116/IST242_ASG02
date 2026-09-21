322e513d0c9310b44ba1930c0c0d3dadeeebb31d
dallas <deast@ithaca.edu>
2026-09-20 20:08:57 -0400

    Added save_library

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
+import json
+
+
 
 
 def display_menu():
     "Display the first menu that appears when the program is run"
     print("\n === Personal Library Manager===")
-    print("1. Add a book title")
+    print("1. Add or update a book title")
     print("2. Remove a book title")
     print("3. List all book titles")
-    print("4. Search for a book title")
-    print("5. Exit")
+    print("4. Search for a book ")
+    print("5. Show Author stats")
+    print("6. Save and exit")
 
+def load_library(filename):
+    "load library from json file"
+    try:
+        with open(filename, 'r') as file:
+            library = json.load(file)
 
+            if isinstance(library, dict):
+                return library
+            else:
+                return {}
+    except FileNotFoundError:
+        return {}
+
+def save_library(library, filename):
+    "save library to json file"
+    with open(filename, 'w') as file:
+        json.dump(library, file, indent=4)
+
+    
 def add_book(library):
     "Add a book title to the library"
     title = input("Enter the book title to add: ")
@@ -38,4 +60,6 @@ def search_book(library):
         print(f"{title} by {author} ({year}) is in your library.")
     else:
         print(f"{title} is not in your library.")
+
+
     
